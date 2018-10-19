6011014c0c3c08727d2345cfa0b67d545ad53713
Geng&l <1933292552@qq.com>
2018-10-19 11:07:01 +0800

    简化代码

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -8,18 +8,9 @@ interface Iprops {
 }
 
 function Title(props: Iprops) {
-    let text: string
-
-    if(props.current_index < 9) {
-        text = '安全风险管控检查信息走势图'
-    }
-    else {
-        text = '劳动安全风险管控图'
-    }
-
     return (
         <div className="Title">
-            {text}
+            {(props.current_index < 9) ? '安全风险管控检查信息走势图' : '劳动安全风险管控图'}
         </div>
     )
 }
