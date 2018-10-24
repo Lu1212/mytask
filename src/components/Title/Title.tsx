2d35e24e171f9514125346f2a8f733eccefe00ef
Geng&l <1933292552@qq.com>
2018-10-24 22:51:19 +0800

    添加一个枚举类型

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,7 +2,7 @@
  *  接收current_index，根据current_index的值决定渲染那一条文本，对应的直线图还是饼图
  *  影响因素：
  *    外部：current_index
- *    内部：19行可修改文本
+ *    内部：17、18行可修改文本
  */
 
 import * as React from 'react';
@@ -13,10 +13,15 @@ interface Iprops {
     current_index: number
 }
 
+enum Text {
+    chart = '安全风险管控检查信息走势图',
+    pie = '劳动安全风险管控图'
+}
+
 function Title(props: Iprops) {
     return (
         <div className="Title">
-            {(props.current_index < 9) ? '安全风险管控检查信息走势图' : '劳动安全风险管控图'}  
+            {(props.current_index < 9) ? Text.chart : Text.pie}  
         </div>
     );
 }
